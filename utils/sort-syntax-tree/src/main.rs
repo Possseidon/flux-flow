b80fb60fd248d6592c04eeb499b871f9ec9a5e6a
Possseidon <xpossseidon@gmail.com>
2023-07-09 19:41:03 +0200

    Fix and improve sort-syntax-tree
    It now works with the new DSL and also operates directly on the file. No more annoying clipboard necessary!

--- utils/sort-syntax-tree/src/main.rs
+++ utils/sort-syntax-tree/src/main.rs
@@ -1,22 +1,46 @@
+use std::{
+    fs::{read_to_string, File},
+    io::{BufWriter, Write},
+    iter::once,
+};
+
+const SYNTAX_TREE_RS_PATH: &str = "flux-flow/src/compiler/parser/syntax_tree.rs";
+
+const SYNTAX_TREE_NODES_START: &str = "syntax_tree_nodes! {";
+const SYNTAX_TREE_NODES_END: &str = "}";
+
 fn main() {
-    let lines = std::io::stdin()
+    let file = read_to_string(SYNTAX_TREE_RS_PATH).unwrap();
+    let mut lines = file.lines();
+
+    assert!(lines.next_back() == Some(SYNTAX_TREE_NODES_END));
+    assert!(lines.next_back() == Some(""));
+
+    let syntax_tree_node_lines = lines
+        .skip_while(|&line| line != SYNTAX_TREE_NODES_START)
+        .skip(1)
+        .collect::<Vec<_>>();
+
+    let mut syntax_tree_nodes = syntax_tree_node_lines
+        .split(|&line| line.is_empty())
+        .collect::<Vec<_>>();
+
+    syntax_tree_nodes.sort_unstable();
+
+    let output = file
         .lines()
-        .collect::<Result<Vec<_>, _>>()
-        .unwrap();
+        .take_while(|&line| line != SYNTAX_TREE_NODES_START)
+        .chain(once(SYNTAX_TREE_NODES_START))
+        .chain(
+            syntax_tree_nodes
+                .into_iter()
+                .flat_map(|group| group.iter().chain(once(&"")))
+                .copied(),
+        )
+        .chain(once(SYNTAX_TREE_NODES_END));
 
-    let mut groups = lines.split(String::is_empty).collect::<Vec<_>>();
-
-    groups.sort_unstable_by_key(|group| {
-        group.first().map(|line| {
-            line.trim_start_matches(|c: char| c.is_ascii_alphanumeric() || c == '_')
-                .trim_start_matches(['!', ' ', '{', '('])
-        })
-    });
-
-    for group in groups {
-        for line in group {
-            println!("{line}");
-        }
-        println!();
+    let mut writer = BufWriter::new(File::create(SYNTAX_TREE_RS_PATH).unwrap());
+    for line in output {
+        writeln!(writer, "{}", line).unwrap();
     }
 }
