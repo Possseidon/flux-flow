7fafea60d5ca2f85b1ad63707ff2c5b6433a2db6
Possseidon <xpossseidon@gmail.com>
2023-07-10 16:59:21 +0200

    Document `grammar.rs`

--- flux-flow/src/compiler/parser/grammar.rs
+++ flux-flow/src/compiler/parser/grammar.rs
@@ -4,17 +4,24 @@ use crate::compiler::lexer::{BraceKind, TokenKind};
 
 use super::syntax_tree_node_builder::{Buildable, NodeBuilder};
 
+/// A grammar that defines parsing rules for a language, somewhat similar to what an EBNF is.
+///
+/// Contains a set of rules, as well as a reference to an initial rule from which parsing starts.
 #[derive(Debug)]
 pub struct Grammar {
     initial_rule: RuleRef,
     rules: Vec<NamedRule>,
 }
 
+/// Used to build up a [Grammar].
 #[derive(Debug, Default)]
 pub struct GrammarBuilder {
     rules: Vec<(TypeId, Option<NamedRule>)>,
 }
 
+/// A named recusrive parsing rule.
+///
+/// It also contains a builder that turns tokens and AST nodes into more AST nodes.
 #[derive(Clone, Debug)]
 pub struct NamedRule {
     pub name: &'static str,
@@ -22,35 +29,48 @@ pub struct NamedRule {
     pub builder: NodeBuilder,
 }
 
+/// A parsing rule that recursively references other rules.
 #[derive(Clone, Debug)]
 pub enum RecursiveRule {
+    /// Parses all given rules in order.
+    ///
+    /// 1. `essential`: Essential rules do not cause an immediate hard error.
+    /// 2. `last_essential`: At least one essential rule must exist.
+    /// 3. `required`: Required rules cause an immediate hard error.
     Concatenation {
         essential: Vec<OptionalRule>,
         last_essential: Rule,
         required: Vec<OptionalRule>,
     },
+    /// Tries to parse the given rules in order until one matches.
     Alternation(Vec<Rule>),
+    /// Repeatedly parses the given rule until it no longer matches.
     Repetition(Rule),
+    /// Special case of a repetition, that consumes the entire token stream.
     GlobalRepetition(Rule),
     BracedRepetition(BraceKind, Rule),
 }
 
+/// A [Rule] that might be either optional or required.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum OptionalRule {
     Optional(Rule),
     Required(Rule),
 }
 
+/// A parsing rule that either parses a single token or references a [NamedRule] in a [Grammar].
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Rule {
     Token(TokenKind),
     Ref(RuleRef),
 }
 
+/// References a [NamedRule] in a [Grammar].
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct RuleRef(usize);
 
 impl Grammar {
+    /// Creates a new `Grammar` instance using the given [Buildable] as an initial rule.
     pub fn new<T: Buildable + Any>() -> Self {
         let mut result = GrammarBuilder::default();
         Grammar {
@@ -63,16 +83,22 @@ impl Grammar {
         }
     }
 
+    /// The rule, where parsing begins.
     pub fn initial_rule(&self) -> RuleRef {
         self.initial_rule
     }
 
+    /// Accesses a [NamedRule] using a [RuleRef].
     pub fn rule(&self, rule_ref: RuleRef) -> &NamedRule {
         &self.rules[rule_ref.0]
     }
 }
 
 impl GrammarBuilder {
+    /// Returns a [RuleRef] for the given [Buildable], creating it if it doesn't exist yet.
+    ///
+    /// This also recursively creates all referenced rules.
+    /// Circular references are perfectly fine. The [GrammarBuilder]'s rules are simply [None].
     pub fn rule<T: Buildable + Any>(&mut self) -> RuleRef {
         if let Some(i) = self
             .rules
